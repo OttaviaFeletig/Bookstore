16319f0fe050e635ad673349363f0395abbd3b29
OttaviaFeletig <ottafel@hotmail.it>
2018-10-30 15:47:29 +0100

    searchbar working, flipping working

--- main.js
+++ main.js
@@ -9,26 +9,37 @@ onload = (() => {
         })
         .then(function (data) {
 
-            console.log(data)
-            console.log(data.books[0])
-//            book = data.books[0];
+            console.log(data.books[0]);
             renderBooksList(data.books)
 //            renderBooksTitle(data.books)
-//            search(data.books)
-            //        hooverTitle(data.books)
-
-
+            search(data.books)
+            activateEvent(data.books)
         })
 })()
 
 
 function getBooksList(book, searchInput) {
+//    console.log(book)
+    var newList = [];
+    console.log(searchInput);
+    if(searchInput == ""){
+        newList = book;
+    } else {
+//        console.log(book)
+        book.forEach(oneBook => {
+            console.log(oneBook)
+            console.log(searchInput)
+            if(oneBook.titulo.toUpperCase().includes(searchInput.toUpperCase()) || oneBook.descripcion.toUpperCase().includes(searchInput.toUpperCase())) {
+                newList.push(oneBook);
+            }
+        })
+    }
 
-    return book.map(oneBook => `<div class="image face col-4 p-3"><img src='${oneBook.portada}' alt="book_cover" class="w-100"><div class="book_title"><h3>${oneBook.titulo}</h3><p>${oneBook.descripcion}</p></div></div>`).join("")
+    return newList.map(oneBook => `<div class="book_container d-flex flex-wrap" ontouchstart="this.classList.toggle('hover');"><div class="book_list col-4"><div class="front p-2"><img src='${oneBook.portada}' alt="book_cover" class="w-100 h-100"></div><div class="back"><h3>${oneBook.titulo}</h3><p>${oneBook.descripcion}</p><button type="button" class="btn" onclick="clickMe()" value='${oneBook.detalle}'>More Info</button></div></div></div>`).join("")
 }
 
 function renderBooksList (book) {
-    document.getElementById("books_list").innerHTML = getBooksList(book)
+    document.getElementById("books_list").innerHTML = getBooksList(book, "")
 }
 
 //function getBooksTitle (book, searchInput) {
@@ -40,19 +51,20 @@ function renderBooksList (book) {
 //   
 //    document.getElementById("books_title").innerHTML = getBooksTitle(book)
 //}
-////
-//function search(book) {
-//    var searchBar = document.getElementById("search_bar").value
-//    console.log(searchBar);
-//    document.getElementById("books_title").innerHTML = getBooksTitle(book, searchBar)
 //
-//}
-//function hooverTitle (book) {
-//    
-//    var elements = document.getElementById("books_title")
-//    var title = renderBooksTitle(book)
-//    
-//    elements.addEventListener("mouseover", function (event) {
-//        return event.title;
-//    })
-//}
+
+function search(book) {
+    var searchBar = document.getElementById("search_bar").value
+    
+    document.getElementById("books_list").innerHTML = getBooksList(book, searchBar) 
+}
+
+function clickMe () {
+    
+    console.log("hi")
+}
+
+function activateEvent (book) {
+     document.querySelector("#search_bar").addEventListener("keyup", () => search(book));
+}
+
